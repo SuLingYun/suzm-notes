2d5463ef50452ae383799243aede42c9991ea2fa
SuLingYun <SuLingYun@users.noreply.github.com>
2026-07-13 13:28:33 +0000

    feat: 添加 Netlify 部署信息
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -10,7 +10,7 @@ export default {
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'footer-before': () => h(Donation),
-      'layout-bottom': () => h(DeployInfo),
+      'layout-bottom': () => h('div', { class: 'footer-meta' }, h(DeployInfo)),
       'layout-top': () => h(FloatingTools)
     })
   }
